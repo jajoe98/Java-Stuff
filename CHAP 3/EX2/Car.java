b2903ae7a9ecb1afef7a708f8ded8693a59a525e
Jacob <jajoe98@gmail.com>
2018-01-19 15:29:24 -0600

    Started CHAP3/EX2 and reverted the .gitignore

---
+++ CHAP 3/EX2/Car.java
@@ -0,0 +1,6 @@
+public class Car
+{
+    public int yearModel;
+    public String make;
+    public int speed;
+}
---
+++ CHAP 3/EX2/EX2.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class EX2
+{
+    
+    
+    public static void main(String[] args)
+    {
+        
+        System.exit(0);
+    }
+}
