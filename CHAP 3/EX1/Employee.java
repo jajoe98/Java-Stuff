de24db3668ad1fd5a218cbb2014317726d887f9a
Jacob <jajoe98@gmail.com>
2018-01-19 15:25:22 -0600

    added .gitignore and did CHAP 3/EX1/

---
+++ CHAP 3/EX1/Employee.java
@@ -0,0 +1,50 @@
+public class Employee
+{
+    //initializers
+    public String name;
+    public int idNumber;
+    public String department;
+    public String position;
+    
+    //mutators
+    public void SetName(String newName)
+    {
+        name = newName;
+    }
+    
+    public void SetId(int id)
+    {
+        idNumber = id;
+    }
+    
+    public void SetDepartment(String newDepartment)
+    {
+        department = newDepartment;
+    }
+    
+    public void SetPosition(String newPosition)
+    {
+        position = newPosition;
+    }
+    
+    //accessors
+    public String GetName()
+    {
+        return name;
+    }
+    
+    public int GetId()
+    {
+        return idNumber;
+    }
+    
+    public String GetDepartment()
+    {
+        return department;
+    }
+    
+    public String GetPosition()
+    {
+        return position;
+    }
+}
